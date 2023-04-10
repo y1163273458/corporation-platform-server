de70f7f16dcfe67f7a8986b0878a0d5bb6baab24
HeDiDiTieTie <y1163273458@163.com>
2023-04-10 15:02:44 +0800

    Initial commit

---
+++ src/main/java/com/example/corporationplatformserver/CorporationPlatformServerApplication.java
@@ -0,0 +1,13 @@
+package com.example.corporationplatformserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CorporationPlatformServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CorporationPlatformServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/corporationplatformserver/CorporationPlatformServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.corporationplatformserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CorporationPlatformServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
